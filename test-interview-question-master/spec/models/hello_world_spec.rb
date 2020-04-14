2071b6481499b323aff404f34265b8319c762bc7
Jigar <singhjigar1391@gmail.com>
2020-04-14 15:02:44 -0500

    update

--- test-interview-question-master/spec/models/hello_world_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
- 
-RSpec.describe 'Hello world', type: :feature do
-  scenario 'index page' do
-    visit root_url
-    sleep(5)
-    expect(page).to have_content('Hello World')
-  end
-end
